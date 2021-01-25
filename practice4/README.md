0588fe1ec872dfae84919adf80038fce88a55474
kuldeep17100 <kuldeep17100@iiitnr.edu.in>
2021-01-25 12:57:46 +0530

    updated

---
+++ practice4/README.md
@@ -0,0 +1,15 @@
+# Forms-Template Driven Forms
+
+## Create Form temlate
+
+## Bind Data with a model
+
+## Perform Validation
+
+## Provide Visual feedback
+
+## Display error message
+
+## Post the data from server
+
+## Receive the response 
---
+++ practice4/src/app/app.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { User } from './user';
+import { EnrollmentService} from './enrollment.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'practice4';
+  public topics=['Angular','React','Vue'];
+  errorMsg = '';
+  topicHasError = true;
+  submitted = false;
+  userModel = new User('Kuldeep','kuldeep@gmail.com',8973856667,'default','morning',true);
+
+  constructor(private _enrollmentService: EnrollmentService){
+
+  }
+
+  validateTopic(value) {
+    if (value === 'default') {
+      this.topicHasError = true;
+    } else {
+      this.topicHasError = false;
+    }
+  }
+  onSubmit(){
+    this.submitted=true;
+    this._enrollmentService.enroll(this.userModel).subscribe(
+      data => console.log('Success!', data),
+      //error => this.errorMsg=error.statusText
+    )
+  }
+
+}
